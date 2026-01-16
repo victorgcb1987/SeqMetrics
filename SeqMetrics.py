f7c65f0d47424389132d989ce216c59b6566b260
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2026-01-16 13:18:13 +0100

    Update SeqMetrics.py

--- SeqMetrics.py
+++ SeqMetrics.py
@@ -27,6 +27,8 @@ def main():
         for kind, dataset in values.items():
             print(kind, key)
             print(dataset)
+    print(datasets["A"]["transcript"]["Stop Chars transcript (%)"].unique())
+    print(datasets["A"]["protein"]["Stop Chars protein (%)"].unique())
     correspondece = get_correspondence_by_blast(Path(argv[5]))
     merged_df = pd.merge(datasets["B"]["transcript"], datasets["B"]["protein"], on="SeqID")
     merged_df = pd.merge(merged_df, correspondece, on="SeqID")
